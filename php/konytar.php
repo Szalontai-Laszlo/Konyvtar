aff368f23d896f5713b9c336ddfe2f6d83e2f5fe
Szalontai-Laszlo <szalilaci10@gmail.com>
2025-10-08 09:19:27 +0200

    konyvtar.php kész

--- php/konytar.php
+++ php/konytar.php
@@ -1,10 +1,24 @@
 <?php
 
-require_once("../common/php/environment.php");
+require_once("../../common/php/environment.php");
 
 $db = new Database("konyvtar");
 
-$query = "";
+$query = "SELECT `authors`.`name` AS 'Szerző',
+                 `title` AS 'Cím',
+                 `categories`.`name` AS 'Kategória',
+                 `borrows`.`borrower_name` AS 'Kölcsönző Neve',
+                 `borrows`.`borrow_date` AS 'Kölcsönzés Dátuma'
+          FROM `books`
+
+          JOIN `authors`
+          ON `books`.`author_id` = `authors`.`id`
+          
+          JOIN `categories`
+          ON `books`.`category_id` = `categories`.`id`
+
+          JOIN `borrows`
+          ON `books`.`id` = `borrows`.`book_id`";
 
 $result = $db -> execute($query);
 
